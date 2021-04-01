b2565b358c25ace43286e8498cc4b02c73522cf9
Juan Gomez <jdavidgomezca@gmail.com>
2021-04-01 12:09:53 -0400

    "First commit"

---
+++ CryptoAlert.py
@@ -0,0 +1,33 @@
+import webbrowser
+import requests
+import json
+
+ApiKey = "92MM9Q3NG1AHGE21YEHMX6F56VWM6GM3PW"
+whale1 = "0xcC64ea842FcDe4283CF239259f7462Ef809c44FD"
+
+# open web with overview of the whale
+whaleOverview = "https://bscscan.com/address/"+whale1
+webbrowser.open(whaleOverview, new=2)
+
+
+def getBalance(whale):
+    #Call balance command, pass whale address and personal BSC APIkey, open browser with balance
+    whaleBalance = "https://api.bscscan.com/api?module=account&action=balance&address="+whale+"&tag=latest&apikey="+ApiKey+".json"
+    webbrowser.open(whaleBalance, new=2)
+
+def getTransactions(whale, interval):
+    #Call tokens transactions command, pass whale address, number of results , and key. open browser with results
+    transactions = "https://api.bscscan.com/api?module=account&action=tokentx&address="+whale+"&page=1&offset="+ str(interval) +"&sort=desc&apikey="+ApiKey+".json"
+    webbrowser.open(transactions, new=2)
+
+    response = requests.get(transactions)
+    DecodedTransactions = json.loads(response.text)
+    print(DecodedTransactions)
+    print(DecodedTransactions['result'][0]['tokenName'])
+
+
+
+getBalance(whale1)
+getTransactions(whale1, 1)
+
+
---
+++ test.py
@@ -0,0 +1,11 @@
+test = {'status': '1',
+ 'message': 'OK',
+  'result': [{'blockNumber': '6111934', 'timeStamp': '1617048483', 'hash': '0x3969d98d5fc68aff0eebaca57ba02a4af755354df2530bcf9004836a77f77dd7',
+   'nonce': '91',
+    'blockHash': '0xb6c41b0f2f1910637c76a2984cf26dcce633c8e26b49f8f3ed4ae809cadb25c2',
+     'from': '0xb25122ab1e0e44d73b7c35e378201787bc9594ed',
+      'contractAddress': '0x69af49e82ea59a97f3879547f67b913c216c3714',
+       'to': '0xcc64ea842fcde4283cf239259f7462ef809c44fd',
+        'value': '9400000', 'tokenName': 'UNITi', 'tokenSymbol': 'UNIT', 'tokenDecimal': '9', 'transactionIndex': '13', 'gas': '154734', 'gasPrice': '10000000000', 'gasUsed': '103156', 'cumulativeGasUsed': '2243418', 'input': 'deprecated', 'confirmations': '80877'}]}
+
+print(test['result'][0]['blockNumber'])
