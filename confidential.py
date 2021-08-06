4d88449d9c0783b912642b1a5ef568863c21cc80
Juan David Gomez Villalba <63566185+Kayaba-Attribution@users.noreply.github.com>
2021-08-06 14:05:50 -0400

    Update confidential.py

--- confidential.py
+++ confidential.py
@@ -21,11 +21,4 @@ def S_Credentials():
 
 
 
-'''
-def random_User():
-    users = ["92MM9Q3NG1AHGE21YEHMX6F56VWM6GM3PW", "EVC2IFRX4NJ2IAMTK592SI8YX9YWPQCZWJ", "A5NDCHK6QBHRCXVSD2JHG9VMDQKPVZYHCE"]
-    magic_number = random.randint(0, 3)
-    return (users[magic_number])
-'''
-#print(get_BSC_API_KEY())
 
